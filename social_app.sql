8e31a305478a4c18578dc2aeb1e55e06b46407fa
mickey014 <kirkmendoza9@gmail.com>
2024-07-26 09:32:29 +0800

    added sql file

--- social_app.sql
+++ social_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 12, 2024 at 02:08 AM
+-- Generation Time: Jul 26, 2024 at 03:31 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -59,7 +59,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 (5, '2024_07_02_071315_create_profiles_table', 1),
-(6, '2024_07_02_080221_create_posts_table', 1);
+(6, '2024_07_02_080221_create_posts_table', 1),
+(8, '2024_07_24_233055_creates_profile_user_pivot_table', 2);
 
 -- --------------------------------------------------------
 
@@ -72,6 +73,13 @@ CREATE TABLE `password_resets` (
   `token` varchar(191) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `password_resets`
+--
+
+INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
+('kirkmendoza9@gmail.com', '$2y$10$1MFeDYbKNnEEHQAcEx8C7.Hq43pfFe2C7iKPgFWruGewndX0Br.ba', '2024-07-25 05:05:53');
 
 -- --------------------------------------------------------
 
@@ -111,10 +119,9 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `user_id`, `created_at`, `updated_at`, `caption`, `image`) VALUES
-(5, 1, '2024-07-07 17:10:17', '2024-07-07 17:10:17', 'test1', 'uploads/fSux0ECpl6mlniNurceldz8osphrXhim2aSHnQpb.webp'),
-(6, 1, '2024-07-07 17:10:35', '2024-07-07 17:10:35', 'test2', 'uploads/A2EvH7m6B2bjmErPJDY7qDdhEHMPiBaP1VUKppUJ.jpg'),
-(7, 1, '2024-07-07 17:10:52', '2024-07-07 17:10:52', 'test3', 'uploads/Zw8fSOjSi76YOSyCFha7qaPnXMgJ23jCPAVLVH1N.jpg'),
-(8, 1, '2024-07-07 17:11:16', '2024-07-07 17:11:16', 'test4', 'uploads/pNX59lnFi8sR5c9LL5grrlwsa3QX1AG52ElB35St.jpg');
+(9, 1, '2024-07-22 17:00:34', '2024-07-22 17:00:34', 'test1', 'uploads/i7d7ykBvdEJ9haEs81I0VU23OqbUcG0RB4stpBfq.png'),
+(10, 3, '2024-07-23 17:27:35', '2024-07-23 17:27:35', 'test3', 'uploads/Aw8hfhVz7C2b3pZCDNJyGW5WetT9RM5R1rIanB3x.jpg'),
+(11, 1, '2024-07-25 16:12:11', '2024-07-25 16:12:11', 'testetst', 'uploads/ieSJPcZSRuIZPmdofcz0PGOgCdolCR4N6PHwuGg2.jpg');
 
 -- --------------------------------------------------------
 
@@ -138,8 +145,33 @@ CREATE TABLE `profiles` (
 --
 
 INSERT INTO `profiles` (`id`, `user_id`, `title`, `description`, `url`, `image`, `created_at`, `updated_at`) VALUES
-(1, 1, 'kirk006', 'Hi I\'m Kirk Anthony Mendoza', 'https://www.skirk.com', NULL, '2024-07-07 16:59:12', '2024-07-10 15:54:22'),
-(2, 2, 'newuser', 'new desdc', 'https://www.skirk2.com', 'uploads/7eVTzsVVhciPIv4ltsKxJVVvWCvqT6cA1FQjvjey.jpg', '2024-07-10 16:03:41', '2024-07-10 16:22:31');
+(1, 1, 'kirk006', 'Hi I\'m Kirk Anthony Mendoza', 'https://www.skirk.com', 'profile/TTV4Z9ZJb1Wa9ihXpQfnlK673O5wPhFdNtLR5oxV.jpg', '2024-07-07 16:59:12', '2024-07-22 17:02:15'),
+(2, 2, 'newuser', 'new desdc', 'https://www.skirk2.com', 'uploads/7eVTzsVVhciPIv4ltsKxJVVvWCvqT6cA1FQjvjey.jpg', '2024-07-10 16:03:41', '2024-07-10 16:22:31'),
+(3, 3, 'test', NULL, NULL, NULL, '2024-07-23 17:20:31', '2024-07-23 17:20:31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile_user`
+--
+
+CREATE TABLE `profile_user` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `profile_id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `profile_user`
+--
+
+INSERT INTO `profile_user` (`id`, `profile_id`, `user_id`, `created_at`, `updated_at`) VALUES
+(44, 3, 3, NULL, NULL),
+(45, 1, 3, NULL, NULL),
+(49, 1, 1, NULL, NULL),
+(50, 3, 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -165,7 +197,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `username`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Kirk Anthony Mendoza', 'kirkmendoza9@gmail.com', 'kirk0065', NULL, '$2y$10$hVrNmvWL0DlzVRl4MOQtBe6mg1wu8m5ewOpn2vVta4hwSskcULuFW', NULL, '2024-07-07 16:59:12', '2024-07-07 16:59:12'),
-(2, 'New User', 'new_user@gmail.com', 'newuser', NULL, '$2y$10$PPEgeCt8aXsDjYoPHTCCXOGpRG0v99OY8PYp/PXFiY1t910mwd.zG', NULL, '2024-07-10 16:03:41', '2024-07-10 16:03:41');
+(3, 'test', 'test@test.com', 'test', NULL, '$2y$10$D9uhnPlbmCHXGpd3jIrMculpvImnXBvNrzavQj4bXKQT25z4txMBm', NULL, '2024-07-23 17:20:31', '2024-07-23 17:20:31');
 
 --
 -- Indexes for dumped tables
@@ -213,6 +245,12 @@ ALTER TABLE `profiles`
   ADD KEY `profiles_user_id_index` (`user_id`);
 
 --
+-- Indexes for table `profile_user`
+--
+ALTER TABLE `profile_user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -234,7 +272,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -246,19 +284,25 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `profiles`
 --
 ALTER TABLE `profiles`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `profile_user`
+--
+ALTER TABLE `profile_user`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
